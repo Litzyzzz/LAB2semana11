cc2c71d3f44bdd1189fa3edd6f087e2e2bd30d7e
Tu Nombre <tuemail@example.com>
2024-10-05 21:49:45 -0600

    listo

--- graficaLineas.py
+++ graficaLineas.py
@@ -2,12 +2,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #Con lineas
-
 df= pd.read_csv("covid19.csv")
-x = df["Date"].value_counts()
-y = df["Confirmed"].value_counts()
+x = df["Date"].head(12)
+y = df["Confirmed"].head(12)
 plt.title("Casos confirmados del COVID19")
 plt.xlabel("Fecha")
 plt.ylabel("Casos Confirmados")
-plt.plot(x,y)
+plt.plot(x,y, color="RED",marker='o')#el tipo marker es para que se visualice mejor la linea
 plt.show()
+
+"""
+Esta grafica nos muestra el aumento de los casos confirmados del
+Covid 19 en el mes de Enero del año 2020, llegando hasta inicio de Febrero
+siendo este el punto más alto desde que se confirmó el primero
+"""
