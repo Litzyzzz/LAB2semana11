a7038ec4d8f436a518ece51cedbbce95922026a1
Tu Nombre <tuemail@example.com>
2024-10-05 21:08:21 -0600

    listo

--- graficaPastel.py
+++ graficaPastel.py
@@ -7,11 +7,17 @@ import pandas as pd
 #con pastel
 df = pd.read_csv("sabrina.csv", delimiter=";")
 #x=df['track_name'].head(7)
-y=df['track_type'].head(7)
+y=df['track_musical_genre']
 counts = y.value_counts()#aqui hace un conteo del tipo de cancion en la columna track type 
-plt.title("Análisis del tipo de pista en 7 canciones de Sabrina Carpenter")
-plt.pie(counts,labels=counts.index, autopct='%1.1f%%', colors=["pink","red","black"])
+plt.title("Géneros Discográfico de Sabrina Carpenter")
+plt.pie(counts,labels=counts.index, autopct='%1.1f%%')
 plt.show()
 
+"""
+Esta gráfica nos dice que aproximadamente un 22% de la 
+discografía de Sabrina Carpenter es de género folk pop, siendo esta
+la mayoria
+
+"""
 
 
